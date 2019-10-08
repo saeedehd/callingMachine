8c1b52ad352f416f565577e917277455018ad94d
SaeedehDeldar <saeedeh.deldar@gmail.com>
2019-10-08 16:27:57 +0330

    test upload-list component

--- src/app/uplaod-list/uplaod-list.component.spec.ts
+++ src/app/uplaod-list/uplaod-list.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UplaodListComponent } from './uplaod-list.component';
+import { FileUploadModule } from 'ng2-file-upload';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UplaodListComponent', () => {
-  let component: UplaodListComponent;
-  let fixture: ComponentFixture<UplaodListComponent>;
+	let component: UplaodListComponent;
+	let fixture: ComponentFixture<UplaodListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UplaodListComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				imports: [ HttpClientModule, NgModule, FileUploadModule ],
+				declarations: [ UplaodListComponent ]
+			}).compileComponents();
+		})
+	);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UplaodListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UplaodListComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
